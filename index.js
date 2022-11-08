b64055bbf8b0d5d8918cf1fe40bdda69c0d89c5c
sartajdev007 <sartaj5akik@gmail.com>
2022-11-09 02:28:17 +0600

    get service api implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -10,6 +11,26 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddhvpui.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const serviceCollection = client.db('dentalWhiz').collection('services')
+    try {
+        // get Homeservices
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => alert("There is an error:", err))
 
 app.get('/', (req, res) => {
     res.send('dental whiz Running')
