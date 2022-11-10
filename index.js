7a65a4db67832e520d7ca94f0e1ec203b71d53cd
sartajdev007 <sartaj5akik@gmail.com>
2022-11-10 11:00:20 +0600

    final commit

--- index.js
+++ index.js
@@ -14,11 +14,35 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddhvpui.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-    const serviceCollection = client.db('dentalWhiz').collection('services')
-    const reviewsCollection = client.db('dentalWhiz').collection('reviews')
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization
+//     if (!authHeader) {
+//         res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     const token = authHeader.split(' ')[1]
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//         if (err) {
+//             res.status(401).send({ message: 'unauthorized access' })
+//         }
+//         req.decoded = decoded
+//         next()
+//     })
+// }
 
+async function run() {
     try {
+        const serviceCollection = client.db('dentalWhiz').collection('services')
+        const reviewsCollection = client.db('dentalWhiz').collection('reviews')
+
+        // jwt token
+        // app.post('/jwt', (req, res) => {
+        //     const user = req.body
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        //     res.send({ token })
+        // })
+
+
+
         // get Homeservices
         app.get('/services', async (req, res) => {
             const query = {};
@@ -42,10 +66,17 @@ async function run() {
 
         // get reviews
         app.get('/reviews', async (req, res) => {
-            const query = {}
+            let query = {}
             const cursor = reviewsCollection.find(query).sort({ time: -1 })
             const reviews = await cursor.toArray()
             res.send(reviews)
+        })
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const review = await reviewsCollection.findOne(query)
+            res.send(review)
         })
 
         app.post('/reviews', async (req, res) => {
@@ -53,6 +84,30 @@ async function run() {
             const result = await reviewsCollection.insertOne(review)
             res.send(result)
         })
+
+        app.put('/reviews/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const newReview = req.body
+            const option = { upsert: true }
+            const updatedReview = {
+                $set: {
+                    review: newReview.review,
+                }
+            }
+            const result = await reviewsCollection.updateOne(query, updatedReview, option)
+            res.send(result)
+        }
+        )
+
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await reviewsCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
     }
     finally {
